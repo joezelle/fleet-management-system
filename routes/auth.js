f6d728e29cb941fc0a87edcc0bbe090764657426
joezelle <51567424+joezelle@users.noreply.github.com>
2019-10-26 15:59:40 +0100

    login and sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,58 +1,56 @@
 const express = require('express');
 
-const User = require('../models/User')
+const bcrypt = require('bcryptjs');
 
-const bcrypt = require('bcryptjs')
+const User = require('../models/User');
 
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-    res.render('login')
-})
+  res.render('auth/login');
+});
+router.post('/login', async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  const foundUser = await User.findOne({ email });
+  if (!foundUser) {
+    res.redirect('auth/signup');
+  }
+  const isMatch = await bcrypt.compare(password, foundUser.password);
+  if (!isMatch) {
+    return res.redirect('auth/login');
+  } else {
+    req.session.isLoggedIn = true;
+    req.session.user = foundUser;
+    await req.session.save();
+    return res.redirect('/');
+  }
+});
 
-router.post('/login', async(req, res) => {
-    let { email, password } = req.body;
-    let foundUser = use.findOne({ email: email })
-    if (!founduser) {
-        res.redirect('/signup')
-    }
-    let isMatch = await bcrypt.compare(password, foundUser.password)
-    if (!isMatch) {
-        res.redirect('back')
-    } else {
-        req.session.isLoggedIn = true;
-        req.session.user = foundUser;
-        await req.session.save()
-        return res.redirect("/")
-    }
-
-})
 router.get('/signup', (req, res) => {
-    res.render('signup')
-})
+  res.render('auth/signup');
+});
 
-router.post('/signup', async(req, res) => {
-    let foundUser = await User.findOne({
-        email: email
-    })
-    if (foundUser) {
-        //TODO; remember to add flash message
-        return res.redirect("/login")
-    } else {
-        let hashedPwd = await bcrypt.hash(password, 12)
-        if (hashedPwd) {
-            const newUser = new User({
-                companyName,
-                email,
-                password: hashedPwd
-            })
-            await newUser.save()
-            return res.redirect("/login")
-        }
-
+router.post('/signup', async (req, res) => {
+  const { email, companyName, password } = req.body;
+  let foundUser = await User.findOne({
+    email: email
+  });
+  if (foundUser) {
+    //TODO; remember to add flash message
+    return res.redirect('/');
+  } else {
+    let hashedPwd = await bcrypt.hash(password, 12);
+    if (hashedPwd) {
+      const newUser = new User({
+        companyName,
+        email,
+        password: hashedPwd
+      });
+      await newUser.save();
+      return res.redirect('/');
     }
-})
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
